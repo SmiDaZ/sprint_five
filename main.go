c05b99cbe2c95c0d2e44faac0a1eb99abf69e15f
SmiDaZ <smidawrk@gmail.com>
2025-01-08 15:38:48 +0300

    second commit

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ type Walking struct {
 func (w Walking) Calories() float64 {
 	// вставьте ваш код ниже
 	return ((CaloriesWeightMultiplier*w.Weight +
-		(math.Pow(w.meanSpeed(), 2.0)/w.Height)*
+		(math.Pow(w.meanSpeed()*KmHInMsec, 2.0)/(w.Height/CmInM))*
 			CaloriesSpeedHeightMultiplier*w.Weight) *
 		w.Duration.Hours() * MinInHours)
 }
@@ -167,6 +167,7 @@ type Swimming struct {
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
 	return float64(s.LengthPool) * float64(s.CountPool) / MInKm / (s.Duration.Hours())
+	//return s.distance() / s.Duration.Hours()
 }
 
 // Calories возвращает количество калорий, потраченных при плавании.
@@ -185,7 +186,7 @@ func (s Swimming) TrainingInfo() InfoMessage {
 	// вставьте ваш код ниже
 	return (InfoMessage{TrainingType: s.TrainingType,
 		Duration: s.Duration,
-		Distance: float64(s.LengthPool) * float64(s.CountPool),
+		Distance: s.distance(),
 		Speed:    s.meanSpeed(),
 		Calories: s.Calories()})
 }
